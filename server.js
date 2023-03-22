962be7433a36b77e4d956f1a89b561802d629583
Ted Bataller <ted.jerbybataller@gmail.com>
2023-03-22 15:15:17 +0800

    finished sever.js setup, added view index

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const server = app.listen(8000);
 const socket = require("socket.io")(server);
 
 app.use(express.static(__dirname));
+app.set("views",__dirname+"/views");
+app.set("view engine","ejs");
+
+app.get("/",function(req, res) {
+    
+})
